bb235e4eaad0eb1b2fcb7392c550025e798d7705
TanveerAhmadRaj <ta78296@gmail.com>
2024-05-08 16:24:49 +0500

    utils with ErrorHandling and Response

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
  import dotenv from "dotenv";
-import connectDB from "./db/index.js"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
  dotenv.config({
     path: "./"
  });
 
- connectDB();
+ connectDB()
+ .then(()=>{
+   app.listen(process.env.PORT || 5000, ()=>{
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+   })
+ })
+ .catch((error)=>{
+   console.log("Database connection is failed!!!!", error);
+ });
